e077d02603bc6f0e746f7636b91162a8368fca25
Maciej Gołaszewski <jodator@gmail.com>
2016-03-09 11:39:14 +0100

    Initial commit

---
+++ public/js/chat.js
@@ -0,0 +1,238 @@
+'use strict';
+
+const app = new Marionette.Application( {} );
+const chatRooms = new ( Backbone.Collection.extend() )();
+const user = new Backbone.Model();
+
+app.rootView = new ( Marionette.LayoutView.extend( {
+	el: '#app',
+	regions: {
+		chats: '#chats',
+		activeChats: '#active-chat-rooms',
+		user: '#user-id',
+		currentChat: '#current-chat'
+	}
+} ) )();
+
+const userView = new ( Marionette.ItemView.extend( {
+	template: '#template-user',
+	events: {
+		'click button': function() {
+			const username = this.$el.find( 'input' ).val().trim();
+
+			if ( !username ) {
+				alert( 'Set username!' );
+
+				return;
+			}
+
+			user.set( 'name', username );
+
+			app.commands.execute( 'connect', user );
+
+			this.disable();
+		}
+	},
+
+	disable: function() {
+		this.$el.find( 'button, input' ).attr( 'disabled', 'disabled' );
+	},
+
+	enable: function() {
+		this.$el.find( 'button, input' ).removeAttr( 'disabled' );
+	}
+} ) )();
+
+const activeChats = new ( Backbone.Collection.extend( {
+	makeActive: function( chat ) {
+		this.forEach( function( chat ) {
+			chat.set( 'isActive', false );
+		} );
+
+		chat.set( 'isActive', true );
+		if ( !chat.has( 'messages' ) ) {
+			chat.set( 'messages', new Backbone.Collection() );
+		}
+
+		app.commands.execute( 'chat:active', chat );
+	}
+} ) )();
+
+const activeChatRoomsView = new Marionette.CollectionView( {
+	collection: activeChats,
+	tagName: 'ul',
+	className: 'nav nav-pills',
+	childView: Marionette.ItemView.extend( {
+		template: '#template-active-chat',
+		tagName: 'li',
+		className: '',
+		events: {
+			'click a': function() {
+				this._parent.collection.makeActive( this.model );
+				this._parent.render();
+			}
+		},
+		onRender: function() {
+			if ( this.model.get( 'isActive' ) ) {
+				this.$el.addClass( 'active' );
+			}
+		}
+	} )
+} );
+
+app.commands.setHandler( 'connect', function( user ) {
+	let socket = io( 'http://localhost:3001' );
+
+	socket.on( 'hello', function( data ) {
+		let chatRooms = [];
+
+		data.chatRooms.forEach( ( name ) => {
+			chatRooms.push( { name: name } );
+		} );
+
+		app.trigger( 'connected', { user: user, chatRooms: chatRooms } );
+		console.log( 'Connected. ' + data.chatRooms.length + ' chats open.' );
+	} );
+
+	socket.on( 'joined', function( data ) {
+		let chatRoom = chatRooms.find( { name: data.chatRoom.name } );
+
+		activeChats.add( chatRoom );
+		activeChats.makeActive( chatRoom );
+		activeChatRoomsView.render();
+	} );
+
+	socket.on( 'chat:join', function( data ) {
+		let chatRoom = chatRooms.find( { name: data.chat } );
+		chatRoom.set( 'isOpen', true );
+
+		if ( !chatRoom.has( 'messages' ) ) {
+			chatRoom.set( 'messages', new Backbone.Collection() );
+		}
+
+		chatRoom.get( 'messages' ).add( _.extend( data, {
+			user: false,
+			time: formatTime( data.time ),
+			msg: 'User ' + data.user + ' joined!'
+		} ) );
+	} );
+
+	socket.on( 'chat:message', function( data ) {
+		let chatRoom = chatRooms.find( { name: data.chat } );
+
+		if ( !chatRoom.has( 'messages' ) ) {
+			chatRoom.set( 'messages', new Backbone.Collection() );
+		}
+
+		if ( data.join ) {
+			_.extend( data, {
+				user: false,
+				msg: 'User ' + data.user + ' joined!'
+			} )
+		}
+
+		data.time = formatTime( data.time );
+
+		chatRoom.get( 'messages' ).add( data );
+	} );
+
+	socket.on( 'disconnect', function() {
+		activeChats.reset();
+		app.rootView.$el.find( '#chats-views' ).hide();
+		activeChatRoomsView.render();
+		userView.enable();
+	} );
+
+	app.commands.setHandler( 'join', function( data ) {
+		socket.emit( 'join', { chatRoom: data.chatRoom.toJSON(), user: user.get( 'name' ) } );
+	} );
+
+	app.commands.setHandler( 'chat:msg', function( data ) {
+		socket.emit( 'chat:msg', { user: user.get( 'name' ), chat: data.chat, msg: data.msg } );
+	} );
+
+	socket.emit( 'hello', { name: user.get( 'name' ) } );
+} );
+
+app.on( 'connected', function() {
+	app.rootView.$el.find( '#chats-views' ).show();
+	app.rootView.activeChats.show( activeChatRoomsView );
+} );
+
+app.commands.setHandler( 'chat:active', function( chat ) {
+	let messages = chat.get( 'messages' );
+
+	let activeChatView = new ( Marionette.CompositeView.extend( {
+		template: '#template-current-chat',
+		childViewContainer: '.chat-room',
+		childView: Marionette.ItemView.extend( {
+			template: '#template-chat-message',
+			className: 'row'
+		} ),
+		events: {
+			'click button': function( evt ) {
+				let input = this.$el.find( 'input' );
+				let text = input.val().trim();
+				input.val( '' );
+				if ( text ) {
+					app.commands.execute( 'chat:msg', { chat: this.model, msg: text } );
+				}
+			}
+		},
+		initialize: function() {
+			var that = this;
+			this.listenTo( messages, 'add', function() {
+				that.render();
+			} );
+		}
+	} ) )( { collection: messages, model: chat } );
+
+	activeChatView.render();
+
+	app.rootView.currentChat.show( activeChatView );
+} );
+
+app.on( 'connected', function( data ) {
+	const chatRoomsView = new Marionette.CollectionView( {
+		collection: chatRooms,
+		className: 'chat-rooms',
+		childView: Marionette.ItemView.extend( {
+			template: '#template-chat',
+			events: {
+				'click button': function() {
+					app.commands.execute( 'join', { chatRoom: this.model } )
+				}
+			}
+		} ),
+		collectionEvents: {
+			'change:isOpen': function() {
+				this.render();
+			}
+		},
+		filter: function( child, index, collection ) {
+			return !child.get( 'isOpen' );
+		}
+	} );
+
+	chatRooms.reset( data.chatRooms );
+	chatRoomsView.render();
+
+	app.rootView.chats.show( chatRoomsView );
+} );
+
+app.rootView.user.show( userView );
+
+app.start();
+
+function formatTime( timeString ) {
+	let time = new Date( timeString );
+	return a( time.getHours() ) + ':' + a( time.getMinutes() ) + ':' + a( time.getSeconds() );
+
+	function a( number ) {
+		if ( number > 9 ) {
+			return number;
+		}
+
+		return '0' + number;
+	}
+}
---
+++ src/server.js
@@ -0,0 +1,135 @@
+'use strict';
+
+const Hapi = require( 'hapi' );
+const path = require( 'path' );
+
+// Hapi server
+const server = new Hapi.Server();
+
+server.connection( { port: 3000, labels: [ 'api' ] } );
+server.connection( { port: 3001, labels: [ 'chat' ] } );
+
+// Socket IO
+const io = require( 'socket.io' )( server.select( 'chat' ).listener );
+
+// Redis Pub/Sub
+const redis = require( 'redis' );
+const pub = redis.createClient();
+const sub = redis.createClient();
+
+const CHAT_ROOMS = 'chatRooms';
+
+//Create sample chat rooms
+//for ( let i = 1; i <= 10; i++ ) {
+//	pub.sadd( CHAT_ROOMS, 'Chat room ' + i );
+//}
+
+server.start( ( err )=> {
+	if ( err ) {
+		throw err;
+	}
+
+	// Show each connection
+	server.connections.forEach( ( connection )=> {
+		console.log( 'Server', connection.settings.labels.join( '-' ), 'running at:', connection.info.uri );
+	} );
+} );
+
+server.register( require( 'inert' ), ( err ) => {
+	if ( err ) {
+		throw err;
+	}
+
+	server.route( {
+		method: 'GET',
+		path: '/',
+		handler: ( request, reply ) => {
+			reply.file( './../public/index.html' );
+		}
+	} );
+
+	server.route( {
+		method: 'GET',
+		path: '/js/{param*}',
+		handler: ( request, reply ) => {
+			reply.file( './../public/js/' + request.params.param )
+		}
+
+	} )
+} );
+
+sub.on( 'message', function( channel, message ) {
+	let data = JSON.parse( message );
+	let chatRoom = data.chat;
+
+	pub.smembers( 'chat:connections:' + chatRoom, ( dunno, socketIds ) => {
+		socketIds.forEach( ( socketId ) => {
+			if ( io.sockets.connected[ socketId ] ) {
+				if ( channel === 'chat:join' ) {
+					io.sockets.connected[ socketId ].emit( 'chat:join', { user: data.user, chat: chatRoom, time: data.time } );
+				} else {
+					io.sockets.connected[ socketId ].emit( 'chat:message', {
+						user: data.user,
+						chat: chatRoom,
+						time: data.time,
+						msg: data.msg
+					} );
+				}
+			}
+		} );
+	} );
+
+} );
+
+io.on( 'connection', ( socket ) => {
+	socket.on( 'hello', () => {
+		pub.smembers( CHAT_ROOMS, ( dunno, chatRooms ) => {
+			socket.emit( 'hello', { chatRooms: chatRooms } );
+		} );
+	} );
+
+	socket.on( 'join', ( data ) => {
+		pub.sadd( 'chat:connections:' + data.chatRoom.name, socket.id );
+		pub.sadd( 'user:chats:' + socket.id, data.chatRoom.name );
+		socket.emit( 'joined', { chatRoom: data.chatRoom, user: data.user } );
+		let message = JSON.stringify( {
+			user: data.user,
+			chat: data.chatRoom.name,
+			time: new Date(),
+			join: true
+		} );
+
+		pub.lrange( 'chat:archive:' + data.chatRoom.name, -5, -1, ( dunno, messages ) => {
+			messages.forEach( ( message )=> {
+				socket.emit( 'chat:message', JSON.parse( message ) );
+			} );
+			pub.rpush( 'chat:archive:' + data.chatRoom.name, message );
+			pub.publish( 'chat:join', message );
+		} );
+	} );
+
+	socket.on( 'disconnect', () => {
+		pub.smembers( 'user:chats:' + socket.id, ( dunno, chatRooms ) => {
+			chatRooms.forEach( ( chatRoom )=> {
+				pub.srem( 'chat:connections:' + chatRoom, socket.id );
+			} );
+		} );
+		pub.del( 'user:chats:' + socket.id );
+	} );
+
+	socket.on( 'chat:msg', ( data )=> {
+		let message = JSON.stringify( {
+			user: data.user,
+			chat: data.chat.name,
+			time: new Date(),
+			msg: data.msg
+		} );
+		// archive
+		pub.rpush( 'chat:archive:' + data.chat.name, message );
+		// publish
+		pub.publish( 'chat:message', message );
+	} );
+} );
+
+sub.subscribe( 'chat:message' );
+sub.subscribe( 'chat:join' );
